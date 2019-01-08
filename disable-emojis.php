efb3322047052c75e88adf4d48381d3d454fd714
johninamillion <johninamio@gmail.com>
2019-01-08 19:29:56 +0100

    Create plugin file

---
+++ disable-emojis.php
@@ -0,0 +1,26 @@
+<?php declare( strict_types = 1 );
+
+/*
+ * Plugin Name: 	Disable Emojis
+ * Plugin URI: 		https://github.com/millionvisions/disable-emojis/
+ * Author: 		    MillionVisions
+ * Author URI: 		https://github.com/millionvisions
+ * License:         MIT
+ * License URI:     https://github.com/millionvisions/portfolio-theme/blob/master/LICENSE
+ * Text Domain:     mv-disable-emojis
+ * Version: 		0.1.0
+ */
+
+/*
+ * This file is part of the 'disable-emojis' package.
+ *
+ * (c) MillionVisions
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+namespace MillionVisions\DisableEmojis;
+
+// return if access directly
+defined( 'ABSPATH' ) or die;
